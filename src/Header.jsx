eeede6ca895bce0fc5b1b9c6d30276882da84f82
Slavik Petryk <slavik.petruk@gmail.com>
2022-02-10 20:40:40 +0200

    1.0.1

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <Link to="/"><div className="header__tab"><img src="./media/tasks.png" alt="tasks" /><p>tasks</p></div></Link>
             <Link to="/calculator"><div className="header__tab"><img src="./media/calculator.png" alt="calculator" /><p>calculator</p></div></Link>
             <Link to="/stopwatch"><div className="header__tab"><img src="./media/stopwatch.png" alt="stopwatch" /><p>stopwatch</p></div></Link>
-            <Link to="/weather"><div className="header__tab"><img src="./media/weather.png" alt="stopwatch" /><p>stopwatch</p></div></Link>
+            <Link to="/weather"><div className="header__tab"><img src="./media/weather.png" alt="weather" /><p>weather</p></div></Link>
             <Link to="/currency"><div className="header__tab"><img src="./media/currencies.png" alt="currency" /><p>currencies</p></div></Link>
             </div>
             
